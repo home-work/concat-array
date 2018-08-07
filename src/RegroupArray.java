57fbb37f82095b16459190eca6b37e3bc7d6cf43
Tien Dat <datdta95@gmail.com>
2018-08-07 17:47:09 +0700

    Nhập vào mảng thứ 1

--- src/RegroupArray.java
+++ src/RegroupArray.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+
 public class RegroupArray {
     public static void main(String[] args) {
-        int[] array_1 = {1, 2, 3, 4, 5};
-        int[] array_2 = {6, 7, 8, 9, 10};
-        int[] array_3 = new int[10];
-        for (int j = 0; j < 5; j++) {
-            array_3[j] = array_1[j];
+        Scanner scanner = new Scanner(System.in);
+        //Nhập mảng thứ 1
+        System.out.println("Nhập vào kích thước của mảng thứ 1:");
+        int sizeOfArray_1= scanner.nextInt();
+        int[] array_1 = new int[sizeOfArray_1];
+        for (int i = 0; i < sizeOfArray_1; i++) {
+            System.out.println("\tNhập vào giá trị của phần tử thứ " + (i + 1));
+            array_1[i] = scanner.nextInt();
         }
-        for (int i = 0; i < 5; i++) {
-            array_3[i + 5] = array_2[i];
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.printf(array_3[i]+"\t");
-        }
+
+
+
+//        int[] array_1 = {1, 2, 3, 4, 5};
+//        int[] array_2 = {6, 7, 8, 9, 10};
+//        int[] array_3 = new int[10];
+//        for (int j = 0; j < 5; j++) {
+//            array_3[j] = array_1[j];
+//        }
+//        for (int i = 0; i < 5; i++) {
+//            array_3[i + 5] = array_2[i];
+//        }
+//        for (int i = 0; i < 10; i++) {
+//            System.out.printf(array_3[i]+"\t");
+//        }
     }
 }
